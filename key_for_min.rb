b7ce2e7bcd02bba7b729ded29e8c0c139c17bb42
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-01 23:49:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,11 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     count += 1 
     if count == 1 
-      
+      min = value 
+    end 
+    if value < min 
+      value = min 
+    end
   end
-
+  
 end
